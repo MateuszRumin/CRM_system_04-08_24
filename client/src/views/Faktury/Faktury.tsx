052e87ab2475cf4007b73a5bf2ac28304d240847
KarolR2001 <115569036+KarolR2001@users.noreply.github.com>
2024-07-03 13:52:15 +0200

    Poprawka sortowania w kolumnach

--- client/src/views/Faktury/Faktury.tsx
+++ client/src/views/Faktury/Faktury.tsx
@@ -4,27 +4,24 @@ import invoices from '../../data/invoices';
 import ThreeDotIcon from '../../../public/icons/3dot.svg';
 import SortIcon from '../../../public/icons/bxs_sort-alt.svg';
 
-interface Invoice {
-	number: string;
-	client: string;
-	type: string;
-	issueDate: string;
-	dueDate: string;
-	amount: string;
-	paymentStatus: string;
-	[key: string]: string; // Indeksowy typ dla stringów
-  }
-
 export const Faktury = () => {
   const [isSearchActive, setIsSearchActive] = useState(false);
   const [searchText, setSearchText] = useState('');
-  const [filteredInvoices, setFilteredInvoices] = useState<Invoice[]>(invoices); // Użycie interfejsu Invoice
+  const [filteredInvoices, setFilteredInvoices] = useState(invoices);
   const [hoveredRow, setHoveredRow] = useState<number | null>(null);
   const [openedInvoiceMenu, setOpenedInvoiceMenu] = useState<string | null>(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(10);
+  const [sortTypeForNumber, setSortTypeForNumber] = useState<'asc' | 'desc' | null>(null);
+  const [sortTypeForClient, setSortTypeForClient] = useState<'asc' | 'desc' | null>(null);
+  const [sortTypeForType, setSortTypeForType] = useState<1 | 2 | 3>(1); // 1 - VAT, 2 - Zaliczka, 3 - Końcowa
+  const [sortTypeForIssueDate, setSortTypeForIssueDate] = useState<'asc' | 'desc' | null>(null);
+  const [sortTypeForDueDate, setSortTypeForDueDate] = useState<'asc' | 'desc' | null>(null);
+  const [sortTypeForAmount, setSortTypeForAmount] = useState<'asc' | 'desc' | null>(null);
+  const [sortTypeForPaymentStatus, setSortTypeForPaymentStatus] = useState<1 | 2 | 3>(1); // 1 - Opłacona, 2 - Oczekuje na płatność, 3 - Nieopłacona
+  const [sortTypeForInvoiceStatus, setSortTypeForInvoiceStatus] = useState<'asc' | 'desc' | null>(null); // Dodane
+
   const itemsPerPageOptions = [10, 20, 30, 50];
-  const [sortConfig, setSortConfig] = useState<{ key: keyof Invoice, direction: string }>({ key: 'number', direction: 'asc' }); // Klucz jako keyof Invoice
 
   useEffect(() => {
     const results = invoices.filter(invoice =>
@@ -56,52 +53,142 @@ export const Faktury = () => {
     }
   };
 
-  const sortTable = (key: keyof Invoice) => {
-	let direction = 'asc';
-	if (sortConfig.key === key && sortConfig.direction === 'asc') {
-	  direction = 'desc';
-	}
-	setSortConfig({ key, direction });
-  
-	// Definicja priorytetów dla różnych typów
-	const priorityOrder: Record<keyof Invoice, number> = {
-	  'Oczekuje na płatność': 1,
-	  'Nieopłacona': 2,
-	  'Opłacona': 3
-	};
-  
-	// Kopiowanie danych, aby nie modyfikować stanu bezpośrednio
-	let sortedInvoices = [...filteredInvoices];
-  
-	sortedInvoices.sort((a, b) => {
-	  if (key === 'type' || key === 'paymentStatus') {
-		// Sortowanie specyficzne dla "Rodzaj Faktury" i "Status płatności"
-		const valueA = priorityOrder[a[key] as keyof Invoice];
-		const valueB = priorityOrder[b[key] as keyof Invoice];
-  
-		if (valueA < valueB) {
-		  return direction === 'asc' ? -1 : 1;
-		}
-		if (valueA > valueB) {
-		  return direction === 'asc' ? 1 : -1;
-		}
-		return 0;
-	  } else {
-		// Dla innych kluczy używamy domyślnego sortowania alfabetycznego
-		const valueA = a[key];
-		const valueB = b[key];
-  
-		if (valueA < valueB) {
-		  return direction === 'asc' ? -1 : 1;
-		}
-		if (valueA > valueB) {
-		  return direction === 'asc' ? 1 : -1;
-		}
-		return 0;
-	  }
-	});
-  
-	setFilteredInvoices(sortedInvoices);
+  const sortByNumber = () => {
+    let sortedInvoices = [...filteredInvoices];
+    if (sortTypeForNumber === 'asc') {
+      sortedInvoices.sort((a, b) => a.number.localeCompare(b.number));
+      setSortTypeForNumber('desc');
+    } else {
+      sortedInvoices.sort((a, b) => b.number.localeCompare(a.number));
+      setSortTypeForNumber('asc');
+    }
+    setFilteredInvoices(sortedInvoices);
+  };
+
+  const sortByClient = () => {
+    let sortedInvoices = [...filteredInvoices];
+    if (sortTypeForClient === 'asc') {
+      sortedInvoices.sort((a, b) => a.client.localeCompare(b.client));
+      setSortTypeForClient('desc');
+    } else {
+      sortedInvoices.sort((a, b) => b.client.localeCompare(a.client));
+      setSortTypeForClient('asc');
+    }
+    setFilteredInvoices(sortedInvoices);
+  };
+
+  const sortByType = () => {
+    let sortedInvoices = [...filteredInvoices];
+    switch (sortTypeForType) {
+      case 1:
+        sortedInvoices.sort((a, b) => {
+          if (a.type === 'VAT') return -1;
+          if (b.type === 'VAT') return 1;
+          return 0;
+        });
+        setSortTypeForType(2);
+        break;
+      case 2:
+        sortedInvoices.sort((a, b) => {
+          if (a.type === 'Zaliczka') return -1;
+          if (b.type === 'Zaliczka') return 1;
+          return 0;
+        });
+        setSortTypeForType(3);
+        break;
+      case 3:
+        sortedInvoices.sort((a, b) => {
+          if (a.type === 'Końcowa') return -1;
+          if (b.type === 'Końcowa') return 1;
+          return 0;
+        });
+        setSortTypeForType(1);
+        break;
+      default:
+        break;
+    }
+    setFilteredInvoices(sortedInvoices);
+  };
+
+  const sortByIssueDate = () => {
+    let sortedInvoices = [...filteredInvoices];
+    if (sortTypeForIssueDate === 'asc') {
+      sortedInvoices.sort((a, b) => new Date(a.issueDate).getTime() - new Date(b.issueDate).getTime());
+      setSortTypeForIssueDate('desc');
+    } else {
+      sortedInvoices.sort((a, b) => new Date(b.issueDate).getTime() - new Date(a.issueDate).getTime());
+      setSortTypeForIssueDate('asc');
+    }
+    setFilteredInvoices(sortedInvoices);
+  };
+
+  const sortByDueDate = () => {
+    let sortedInvoices = [...filteredInvoices];
+    if (sortTypeForDueDate === 'asc') {
+      sortedInvoices.sort((a, b) => new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime());
+      setSortTypeForDueDate('desc');
+    } else {
+      sortedInvoices.sort((a, b) => new Date(b.dueDate).getTime() - new Date(a.dueDate).getTime());
+      setSortTypeForDueDate('asc');
+    }
+    setFilteredInvoices(sortedInvoices);
+  };
+
+  const sortByAmount = () => {
+    let sortedInvoices = [...filteredInvoices];
+    if (sortTypeForAmount === 'asc') {
+      sortedInvoices.sort((a, b) => parseFloat(a.amount) - parseFloat(b.amount));
+      setSortTypeForAmount('desc');
+    } else {
+      sortedInvoices.sort((a, b) => parseFloat(b.amount) - parseFloat(a.amount));
+      setSortTypeForAmount('asc');
+    }
+    setFilteredInvoices(sortedInvoices);
+  };
+
+  const sortByPaymentStatus = () => {
+    let sortedInvoices = [...filteredInvoices];
+    switch (sortTypeForPaymentStatus) {
+      case 1:
+        sortedInvoices.sort((a, b) => {
+          if (a.paymentStatus === 'Opłacona') return -1;
+          if (b.paymentStatus === 'Opłacona') return 1;
+          return 0;
+        });
+        setSortTypeForPaymentStatus(2);
+        break;
+      case 2:
+        sortedInvoices.sort((a, b) => {
+          if (a.paymentStatus === 'Oczekuje na płatność') return -1;
+          if (b.paymentStatus === 'Oczekuje na płatność') return 1;
+          return 0;
+        });
+        setSortTypeForPaymentStatus(3);
+        break;
+      case 3:
+        sortedInvoices.sort((a, b) => {
+          if (a.paymentStatus === 'Nieopłacona') return -1;
+          if (b.paymentStatus === 'Nieopłacona') return 1;
+          return 0;
+        });
+        setSortTypeForPaymentStatus(1);
+        break;
+      default:
+        break;
+    }
+    setFilteredInvoices(sortedInvoices);
+  };
+
+  const sortByInvoiceStatus = () => {
+    let sortedInvoices = [...filteredInvoices];
+    if (sortTypeForInvoiceStatus === 'asc') {
+      sortedInvoices.sort((a, b) => a.invoiceStatus.localeCompare(b.invoiceStatus));
+      setSortTypeForInvoiceStatus('desc');
+    } else {
+      sortedInvoices.sort((a, b) => b.invoiceStatus.localeCompare(a.invoiceStatus));
+      setSortTypeForInvoiceStatus('asc');
+    }
+    setFilteredInvoices(sortedInvoices);
   };
 
   const indexOfLastInvoice = currentPage * itemsPerPage;
@@ -151,90 +238,46 @@ export const Faktury = () => {
         <table className={styles.table}>
           <thead>
             <tr>
-			<th>
-				<div className={styles.headerCell} onClick={() => sortTable('number')}>
-					Numer faktury
-					<img
-					src={SortIcon}
-					alt="Sort"
-					className={`${styles.sortIcon} ${
-						sortConfig.key === 'number' ? (sortConfig.direction === 'desc' ? styles.desc : '') : ''
-					}`}
-					/>
-				</div>
-				</th>
-				<th>
-				<div className={styles.headerCell} onClick={() => sortTable('client')}>
-					Klient
-					<img
-					src={SortIcon}
-					alt="Sort"
-					className={`${styles.sortIcon} ${
-						sortConfig.key === 'client' ? (sortConfig.direction === 'desc' ? styles.desc : '') : ''
-					}`}
-					/>
-				</div>
-				</th>
-				<th>
-				<div className={styles.headerCell} onClick={() => sortTable('type')}>
-					Rodzaj Faktury
-					<img
-					src={SortIcon}
-					alt="Sort"
-					className={`${styles.sortIcon} ${
-						sortConfig.key === 'type' ? (sortConfig.direction === 'desc' ? styles.desc : '') : ''
-					}`}
-					/>
-				</div>
-				</th>
-				<th>
-				<div className={styles.headerCell} onClick={() => sortTable('issueDate')}>
-					Data Wystawienia
-					<img
-					src={SortIcon}
-					alt="Sort"
-					className={`${styles.sortIcon} ${
-						sortConfig.key === 'issueDate' ? (sortConfig.direction === 'desc' ? styles.desc : '') : ''
-					}`}
-					/>
-				</div>
-				</th>
-				<th>
-				<div className={styles.headerCell} onClick={() => sortTable('dueDate')}>
-					Termin płatności
-					<img
-					src={SortIcon}
-					alt="Sort"
-					className={`${styles.sortIcon} ${
-						sortConfig.key === 'dueDate' ? (sortConfig.direction === 'desc' ? styles.desc : '') : ''
-					}`}
-					/>
-				</div>
-				</th>
-				<th>
-				<div className={styles.headerCell} onClick={() => sortTable('amount')}>
-					Kwota
-					<img
-					src={SortIcon}
-					alt="Sort"
-					className={`${styles.sortIcon} ${
-						sortConfig.key === 'amount' ? (sortConfig.direction === 'desc' ? styles.desc : '') : ''
-					}`}
-					/>
-				</div>
-				</th>
-				<th>
-				<div className={styles.headerCell} onClick={() => sortTable('paymentStatus')}>
-					Status płatności
-					<img
-					src={SortIcon}
-					alt="Sort"
-					className={`${styles.sortIcon} ${
-						sortConfig.key === 'paymentStatus' ? (sortConfig.direction === 'desc' ? styles.desc : '') : ''
-					}`}
-					/>
-				</div>
-				</th>
+              <th onClick={sortByNumber}>
+                <div className={styles.headerCell}>
+                  Numer faktury <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
+              <th onClick={sortByClient}>
+                <div className={styles.headerCell}>
+                  Klient <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
+              <th onClick={sortByType}>
+                <div className={styles.headerCell}>
+                  Rodzaj Faktury <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
+              <th onClick={sortByInvoiceStatus}>
+                <div className={styles.headerCell}>
+                  Status Faktury <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
+              <th onClick={sortByIssueDate}>
+                <div className={styles.headerCell}>
+                  Data Wystawienia <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
+              <th onClick={sortByDueDate}>
+                <div className={styles.headerCell}>
+                  Termin płatności <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
+              <th onClick={sortByAmount}>
+                <div className={styles.headerCell}>
+                  Kwota <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
+              <th onClick={sortByPaymentStatus}>
+                <div className={styles.headerCell}>
+                  Status płatności <img src={SortIcon} alt="Sort" className={styles.sortIcon} />
+                </div>
+              </th>
               <th></th>
             </tr>
           </thead>
@@ -249,6 +292,7 @@ export const Faktury = () => {
                 <td>{invoice.number}</td>
                 <td>{invoice.client}</td>
                 <td>{invoice.type}</td>
+                <td>{invoice.invoiceStatus}</td>
                 <td>{invoice.issueDate}</td>
                 <td>{invoice.dueDate}</td>
                 <td>{invoice.amount}</td>
@@ -278,28 +322,28 @@ export const Faktury = () => {
           </tbody>
         </table>
         <div className={styles.paginationContainer}>
-        <div className={styles.itemsPerPage}>
-          <select value={itemsPerPage} onChange={(e) => changeItemsPerPage(parseInt(e.target.value))}>
-            {itemsPerPageOptions.map(option => (
-              <option key={option} value={option}>{option}</option>
+          <div className={styles.itemsPerPage}>
+            <select value={itemsPerPage} onChange={(e) => changeItemsPerPage(parseInt(e.target.value))}>
+              {itemsPerPageOptions.map(option => (
+                <option key={option} value={option}>{option}</option>
+              ))}
+            </select>
+            <span className={styles.itemsPerPageText}> / na stronę</span>
+          </div>
+          <div className={styles.pagination}>
+            <span className={styles.itemsPerPageText}>Poprzednia</span>
+            {Array.from({ length: Math.ceil(filteredInvoices.length / itemsPerPage) }, (_, i) => (
+              <button
+                key={i + 1}
+                onClick={() => paginate(i + 1)}
+                className={`${styles.pageLink} ${currentPage === i + 1 ? styles.active : ''}`}
+              >
+                {i + 1}
+              </button>
             ))}
-          </select>
-          <span className={styles.itemsPerPageText}> / na stronę</span>
+            <span className={styles.itemsPerPageText}>Następna</span>
+          </div>
         </div>
-        <div className={styles.pagination}>
-          <span className={styles.itemsPerPageText}>Poprzednia</span>
-          {Array.from({ length: Math.ceil(filteredInvoices.length / itemsPerPage) }, (_, i) => (
-            <button
-              key={i + 1}
-              onClick={() => paginate(i + 1)}
-              className={`${styles.pageLink} ${currentPage === i + 1 ? styles.active : ''}`}
-            >
-              {i + 1}
-            </button>
-          ))}
-          <span className={styles.itemsPerPageText}>Następna</span>
-        </div>
-      </div>
         {openedInvoiceMenu !== null && (
           <div className={styles.absoluteMenu}>
             <div className={styles.menu}>
